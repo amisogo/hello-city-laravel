db192f9fcaa4898e38d5ea09424654d6b71948ce
Aminata Sogodogo <amisogo0804.gmail.com>
2020-12-14 12:46:11 +0000

    Update homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello City</title>
+
+
+
+    </head>
+    <body>
+        <h1>Hello from Mali!</h1>
+
+        <p> It's currently {{ date('h:i A')}}.</p>
+
+        <footer>&copy; Copyright {{ date('Y')}}  &middot; <a href="#">About Us</a> </footer>
+
+    </body>
+</html>
